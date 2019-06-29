1099d4ad0c78c6593516865060c141f95944887b
Samantha Branham <glompix@gmail.com>
2019-06-29 08:11:45 -0700

    auto-refresh status

--- www/src/App.js
+++ www/src/App.js
@@ -5,9 +5,7 @@ import ServerStats from './ServerStats';
 export default class App extends React.Component {
   render() {
     return (<div className="App">
-      <header>
-        <ServerStats/>
-      </header>
+      <ServerStats/>
     </div>);
   }
 }
