15a633585edefd59d52bd337828381e044cfedf0
Samantha Branham <sbranham@apple.com>
2019-06-29 03:59:02 -0700

    convert to react

---
+++ www/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>minecraft.glompix.com</h1>
+        <p>1.14 - survival -&nbsp;
+          <a href="mailto:glompix@gmail.com">glompix@gmail.com</a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
