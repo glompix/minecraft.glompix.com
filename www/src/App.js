03dadadf4d994e978751c78a144dcfc3e6df6f42
Samantha Branham <sbranham@apple.com>
2019-06-29 05:04:53 -0700

    Server status

--- www/src/App.js
+++ www/src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
+import ServerStats from './ServerStats';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>minecraft.glompix.com</h1>
-        <p>1.14 - survival -&nbsp;
-          <a href="mailto:glompix@gmail.com">glompix@gmail.com</a>
-        </p>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>minecraft.glompix.com</h1>
+          <ServerStats/>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
